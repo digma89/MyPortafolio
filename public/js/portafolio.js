a835e142925de898cfc495933e49f6599d243e8d
digma89 <digma89@hotmail.com>
2016-12-23 20:36:20 -0500

    V.2.0.0 started about

--- public/js/portafolio.js
+++ public/js/portafolio.js
@@ -1,7 +1,4 @@
         $(document).ready(function() {
-
-
-            console.log("test1");
             $('#fullpage').fullpage({
                 verticalCentered: true,
                 sectionsColor: ['#34394E', '#34394E', '#D7E3ED', '#34394E'],
